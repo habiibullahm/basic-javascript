2967c4e42e948638250f480d4a2329f9d410f8b5
Muhammad Habibullah <Muhammadhabibullah32@gmail.com>
2021-12-13 00:28:07 +0700

    data collection array with object

---
+++ collections.js
@@ -0,0 +1,28 @@
+let mobil = [
+    {
+      name: "brio",
+      brand: "honda",
+      speed: 80,
+    },
+    {
+      name: "L300",
+      brand: "mitsubishi",
+      speed: 20,
+    },
+    {
+      name: "M2",
+      brand: "BMW",
+      speed: 150,
+    },
+  ];
+  function getSummary(data) {
+    return `Mobil ${data.brand} ${data.name}, kecepatan maximal ${data.speed} km/jam`;
+  }
+  mobil.map((item) => {
+    console.log(getSummary(item));
+  });
+  
+//   result : 
+//   Mobil honda brio, kecepatan maximal 80 km/jam
+//   Mobil mitsubishi L300, kecepatan maximal 20 km/jam
+//   Mobil BMW M2, kecepatan maximal 150 km/jam
